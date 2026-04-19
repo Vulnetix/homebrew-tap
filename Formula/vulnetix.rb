ad64255c3964229150efa970d09580accfc44d0e
Christopher Langton <chris@vulnetix.com>
2026-04-19 11:33:03 +0000

    vulnetix 2.0.4

--- Formula/vulnetix.rb
+++ Formula/vulnetix.rb
@@ -1,28 +1,28 @@
 class Vulnetix < Formula
   desc "Vulnetix Unified Command Line Utility for the Vulnerability Database"
   homepage "https://github.com/Vulnetix/cli"
-  version "2.0.3"
+  version "2.0.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Vulnetix/cli/releases/download/v#{version}/vulnetix-darwin-arm64"
-      sha256 "6034b1326d8c5f102bd9e702aea29d58e565438edde161ddd80ab11381e636f5"
+      sha256 "63164d50c6c99c86af8d290fd16f9245b321e0948c64ab16f531b8157031a871"
     end
     on_intel do
       url "https://github.com/Vulnetix/cli/releases/download/v#{version}/vulnetix-darwin-amd64"
-      sha256 "f9ce98033ac3764f8deab02e357e10ec3aa4e1bd715c2685a9b47fca7f417faf"
+      sha256 "702f6b1097addb72b4afaf6172a9c1f6b6b40dd4d4b9f811399f4842fac9344b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Vulnetix/cli/releases/download/v#{version}/vulnetix-linux-arm64"
-      sha256 "898ac4fa0b9cc518f8294a5f11f7cff40d21ffb759c6b72d7d943dec4026a258"
+      sha256 "4ab3f8800f73b5693edac993acb958a45c91e7a1eec6a63a98f8ffc1c1d0702f"
     end
     on_intel do
       url "https://github.com/Vulnetix/cli/releases/download/v#{version}/vulnetix-linux-amd64"
-      sha256 "1476e38f857d6b4eee5590bb94b5865bf0c00328575e25e1e0baf439bafcb3f3"
+      sha256 "8faf37af1a03abbf12584bc5a39e3e5d31cbed6657658fea43417b540a3bcfa4"
     end
   end
 
